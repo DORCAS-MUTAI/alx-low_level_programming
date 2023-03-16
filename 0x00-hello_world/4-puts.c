ea9721b51a19f4db6fb0629fd6d262a84d9bc355
Dorcas <dorcasjeropmutai@gmail.com>
2023-03-16 03:56:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main-main body
- * retursn value 0
+ * return: value 0
  */
 
 
-int main (void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
