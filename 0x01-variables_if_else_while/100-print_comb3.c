8db8cb2be0aa1c67362b84f755b00c310d0876d3
Dorcas <dorcasjeropmutai@gmail.com>
2023-03-17 23:10:41 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 00; i <= 90; i++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar('');
+	}
+	putchar('\n'0;
+	return (0);
+}
+
