8597a9678dbdad69a0eba5a90b1bd87bb8ef3dc7
Dorcas <dorcasjeropmutai@gmail.com>
2023-03-17 23:31:42 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 138; i++)
+	for (i = 0; i < 100; i++)
 	{
-		putchar(i);
-		if (i == 138)
+		putchar(i + '0');
+		if (i == 100)
 		{
 			break;
 		}
