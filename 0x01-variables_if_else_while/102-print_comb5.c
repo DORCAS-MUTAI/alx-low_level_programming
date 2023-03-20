79d6c846b9bbe4396e253d6bce11689b66172158
Dorcas <dorcasjeropmutai@gmail.com>
2023-03-20 02:06:37 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,9 +15,12 @@ int main(void)
 		{
 			if (i < j)
 			{
-				putchar(i + '0');
-				putchar(j + '0');
-				if (j + i != 18)
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
