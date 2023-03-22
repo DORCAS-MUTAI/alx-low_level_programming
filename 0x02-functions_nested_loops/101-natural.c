4b3d817142ef67bd03c8276e46c6fb97b1ab6cdb
Dorcas <dorcasjeropmutai@gmail.com>
2023-03-22 10:03:20 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Return: returns 0
+ *
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (((i % 3) == 0) || ((i % 5) == 0))
+		{
+			sum = sum + 1;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
