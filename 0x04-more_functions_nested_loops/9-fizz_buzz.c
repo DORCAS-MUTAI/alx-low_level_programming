ffce4e66931613dbcd3b81047e016f67aeb96cb1
Dorcas <dorcasjeropmutai@gmail.com>
2023-03-23 09:14:35 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,29 +8,28 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf(" Fizz");
 		}
-			if (i % 5 == 0 && i % 3 != 0)
-			{
-				printf(" Buzz");
-			}
-			else if (i % 5 == 0 && i % 3 == 0)
-			{
-				printf(" FizzBuzz");
-			}
-			/**else if (i == 1)
-			*{
-			*	printf("%d", i);
-			*}
-			*/
-			else
-			{
-				printf(" %d", i);
-			}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
 	}
 	printf("\n");
 	return (0);
