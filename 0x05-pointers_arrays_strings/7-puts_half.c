60f6f1774f5f3ebbf5340e9038a2d546568fe3ff
Dorcas <dorcasjeropmutai@gmail.com>
2023-03-27 08:18:28 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/*
+*puts_half - prints half of a string followed by a new line
+*@str: input
+*Return: always 0
+*
+*
+*/
+
+void puts_half(char *str)
+{
+	int a, n, string;
+
+	string = 0;
+
+	for (a = 0; str[a] != 0; a++)
+	{
+		string++;
+	}
+	n = (string / 2);
+	if ((string % 2) == 1)
+		n = ((string + 1) / 2);
+	for (a = n; str[a] != 0; a++)
+		_putchar(str[a];
+	_putchar('\n');
+}
