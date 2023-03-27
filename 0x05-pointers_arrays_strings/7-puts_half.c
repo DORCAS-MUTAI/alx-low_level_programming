218d5f4fc0c8ce47f22f6be1cb52f2f1d4fa1ee7
Dorcas <dorcasjeropmutai@gmail.com>
2023-03-27 09:15:45 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,18 +9,20 @@
 
 void puts_half(char *str)
 {
-	int a, n, string;
+	int a, n, longi;
 
-	string = 0;
+	longi = 0;
 
-	for (a = 0; str[a] != 0; a++)
-	{
-		string++;
-	}
-	n = (string / 2);
-	if ((string % 2) == 1)
-		n = ((string + 1) / 2);
-	for (a = n; str[a] != 0; a++)
+	for (a = 0; str[a] != '\0'; a++)
+		longi++;
+
+	n = (longi / 2);
+
+	if ((longi % 2) == 1)
+		n = ((longi + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
 		_putchar(str[a]);
 	_putchar('\n');
 }
+
