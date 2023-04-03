8ed2e0874ae759a00e0939ba97dadb7cdfc44e80
Dorcas <dorcasjeropmutai@gmail.com>
2023-04-03 12:06:27 -0700

    zero

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+**_memset - fills memory with constant byte
+*@s: address of memory to print
+*@b: the constant byte
+*@n: size of memory to print
+*
+*
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int r = 0;
+
+	for (r = 0; r < n; r++)
+		s[r] = b;
+	return (s);
+}
