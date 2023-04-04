97def473d75107b2ff4497fcb1ceb3ad56f98382
Dorcas <dorcasjeropmutai@gmail.com>
2023-04-04 10:30:44 -0700

    8888

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+*print_diagsums - entry point
+*@a: input
+*@size: input
+*Return: always 0
+*
+*
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int sum, sum0, i;
+
+	sum = 0;
+	sum0 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum = sum + a[i * size + i];
+	}
+	for (i = size - 1; i >= 0; i--)
+	{
+		sum0 += a[i * size + (size - i - 10)];
+	}
+	printf("%d, %d\n", sum, sum0);
+}
+
