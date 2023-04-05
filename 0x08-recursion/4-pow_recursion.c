e8981b0e3f0373935f2c2105b05f3bec15809ced
Dorcas <dorcasjeropmutai@gmail.com>
2023-04-05 09:54:11 -0700

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*_pow_recursion - power x
+*@x: num1
+*@y: num2
+*Return: power of x raised to power of y
+*
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x *_pow_recursion(x, y - 1));
+}
