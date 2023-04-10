f581017e98d5a9a221a51467359651ad115a6c31
Dorcas <dorcasjeropmutai@gmail.com>
2023-04-10 14:03:20 -0700

    print your name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - entry point
+*@argc - count of the arguments in a program
+*@argv - Array of pointer to char
+*Return: 0
+*
+*/
+
+int main(int argc, char **argv)
+{
+(void)argc;
+
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Arguments beginns here
