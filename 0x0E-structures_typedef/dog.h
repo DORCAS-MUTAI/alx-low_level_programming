5fef74c41ebee69c7e2ebb2217090eb511ec14f9
Dorcas <dorcasjeropmutai@gmail.com>
2023-04-17 02:54:18 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 /**
+*struct dog - dogs basic info
 *@name: parameter 1
 *@age: parameter 2
 *@owner: parameter 3
-*
+*Description: longer description
 */
 struct dog
 {
