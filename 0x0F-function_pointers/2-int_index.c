38f6b25d0a75ee7caecba0e4f5150410b9439725
Dorcas <dorcasjeropmutai@gmail.com>
2023-04-19 10:14:10 -0700

    search for a interger

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+*int_index - returns index of the first element
+*@array:
+*@size: number of elements in the array
+*@cmp: pointer to compare values
+*Return: 0 if true
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
