e6d8a8b43c3647c4e56b0799215d86679284f688
DORCAS-MUTAI <dorcasjmutai@gmail.com>
2023-08-21 10:53:07 -0700

    libdynamic

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - prints an absolute integer
+ * @n: value to be checked
+ * Return: absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
+
